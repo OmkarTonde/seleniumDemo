c535f4d2b40ee8d00b70b1a60c0ad1f547337df7
OmkarTonde <omkar.tonde0@gmail.com>
2020-12-26 08:39:20 +0530

    first commit

---
+++ JenkinsDemo/src/test/java/JenkinsDemoPack/JenkinsDemo.java
@@ -0,0 +1,14 @@
+package JenkinsDemoPack;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+	
+  @Test
+  public void jenkinsTest() {
+	  
+	  System.out.println("Jenkins Test");
+	  
+  }
+}
+	
---
+++ JenkinsDemo/src/test/java/JenkinsDemoPack/hellejenkins.java
@@ -0,0 +1,12 @@
+package JenkinsDemoPack;
+
+public class hellejenkins {
+
+	private void sysotu() {
+
+		
+		System.out.println("hello frnds from jenkins");
+
+	}	
+	
+}
